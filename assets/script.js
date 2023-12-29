37b88ecb793209ede3a26194ab499435f56a8caa
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2023-12-29 18:30:17 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,20 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+document.querySelector('.arrow_left').addEventListener('click', function() {
+    console.log('Flèche gauche cliquée');
+});
+
+document.querySelector('.arrow_right').addEventListener('click', function() {
+    console.log('Flèche droite cliquée');
+});
+
+const dotsContainer = document.querySelector('.dots');
+console.log(dotsContainer.classList);
+slides.forEach((slide, index) => {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    if (index === 0) dot.classList.add('dot_selected');
+    dotsContainer.appendChild(dot);
+});
